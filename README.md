359fbf2b0f3b88e9eaece51dcc1781f7307fa5e3
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2025-06-13 22:02:29 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Prefect TOML error
+
+Run `uv run prefect --help` in the project root directory to see the error.
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from prefect-toml-error!")
+
+
+if __name__ == "__main__":
+    main()
